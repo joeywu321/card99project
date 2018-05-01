6d3c2d4888c9ae31fcb346b09de5df2b993103dc
joeywu321 <joeywu321@yahoo.com.tw>
2018-05-01 23:12:01 +0800

    no message

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ if (mongoURL == null ) {
         //mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
         //mongoPassword = process.env[mongoServiceName + '_PASSWORD']
         //mongoUser = process.env[mongoServiceName + '_USER'];
-    var mongoHost = "127.0.0.1",
+    var mongoHost = "mongodb-card99project.7e14.starter-us-west-2.openshiftapps.com";	//"127.0.0.1",
         mongoPort = 27017,
         mongoDatabase = "sampledb",
         mongoPassword = "bdPN8vvFe5HYhLXb",
