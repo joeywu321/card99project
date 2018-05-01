edf95dca0a0230c96f7fccf4a4e9d056e3f592cd
joeywu321 <joeywu321@yahoo.com.tw>
2018-05-01 23:18:41 +0800

    no message

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ if (mongoURL == null ) {
         }
         // Provide UI label that excludes user id and pw
         mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-        mongoURL += mongoHost + ':' +  mongoPort ;// mongoDatabase;
+        mongoURL += mongoHost + ':' +  mongoPort + '/' + "mongodb-persistent";// mongoDatabase;
     }
 }
 var db = null,
